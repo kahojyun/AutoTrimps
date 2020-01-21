2e0740171945aca22f751bb2dadd7d5edf03a3e1
kahojyun <kaho0769@qq.com>
2020-01-22 04:24:49 +0800

    Hide special modifier message

--- modules/maps.js
+++ modules/maps.js
@@ -107,7 +107,7 @@ function testMapSpecialModController() {
                 }
                 var d = updateMapCost(!0),
                     e = game.resources.fragments.owned;
-                "0" != c.value && debug("Set the map special modifier to: " + mapSpecialModifierConfig[c.value].name + ". Cost: " + (100 * (d / e)).toFixed(2) + "% of your fragments.");
+                "0" != c.value && getPageSetting('SpamMaps') && debug("Set the map special modifier to: " + mapSpecialModifierConfig[c.value].name + ". Cost: " + (100 * (d / e)).toFixed(2) + "% of your fragments.");
             }
             var g = getSpecialModifierSetting(),
                 h = 109 <= game.global.highestLevelCleared,
